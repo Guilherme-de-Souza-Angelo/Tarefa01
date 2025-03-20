4cd05ee5749e25a23170db45eed4866754fc06b3
unknown <2314290132@ACADEMICO.IESB.BR>
2025-03-20 10:37:26 -0300

    Tarefa01

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, Image } from 'react-native';
+class App extends Component {
+    render() {
+      return(
+          <View>
+            <Text style={{color: '#4B0082', fontSize: 30, margin: 10}}>Jogos</Text>
+            <Image
+            source={{ uri: 'https://upload.wikimedia.org/wikipedia/pt/9/99/Super_Mario_Odyssey_Capa.png'}}
+            style={{width: 150, height: 250}}
+          />
+            <Text>Nome: Super Mario Odyssey</Text>
+            <Text>Lançamento: 27 de outubro de 2017</Text>
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+            <Image
+            source={{ uri: 'https://upload.wikimedia.org/wikipedia/pt/e/ef/Super_Mario_Maker_2_capa.png'}}
+            style={{width: 150, height: 250}}
+            
+          />
+          <Text>Super Mario Maker 2</Text>
+          <Text>Lançamento:28 de junho de 2019</Text>
+          </View>
+      );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
